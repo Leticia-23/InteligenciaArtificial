9791ac37690b51fdc086e6b8a628faef673aedbf
Leticia-23 <760549@unizar.es>
2021-12-09 18:25:54 +0100

    pocos avances tp6

--- aimacode/aima-core/src/main/java/aima/core/search/adversarial/MinimaxArbolJuego.java
+++ aimacode/aima-core/src/main/java/aima/core/search/adversarial/MinimaxArbolJuego.java
@@ -1,5 +1,10 @@
 package aima.core.search.adversarial;
 
+import aima.core.search.adversarial.ArboldeJuego;
+
 public class MinimaxArbolJuego {
 
+		public static void  minimax(ArboldeJuego<Double> arbol) {
+			
+		}
 }
